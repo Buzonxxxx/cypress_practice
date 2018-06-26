4cd5afa33d83d5a577d82016ed156a6adca6bb6e
Louis Liao <buzonliao@gmail.com>
2018-06-26 13:46:44 +0800

    adjust timeout

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,14 +1,12 @@
-describe('My First Test', () => {
-    it('Visit google search!', () => {
+describe('錢包', () => {
+    it.skip('should create new wallet', () => {
         cy.visit('https://staging.xpa.exchange/#/exchange')
-        cy.get('.button > span').contains("創建錢包").click()
-        
+        cy.get('ul > :nth-child(2) > span').click()
         cy.get('.modal-input').within(() => {
             cy.get('input').type('esthertsai1204')            // Only yield inputs within form
         })
-
         cy.get('.modal-btn .button').contains("創建").click()
-        cy.wait(30000)
+        cy.wait(15000)
         cy.get('.button').contains("匯出錢包") 
     })
   })
